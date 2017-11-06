cd28cdeded28f61d1f16f7b2d868fac60aa5a8c9
Chris Mc <prince.chrismc@gmail.com>
2017-11-05 20:22:33 -0500

    adding dbg print to find issue

--- Assignments/A2/quicksort-par.c
+++ Assignments/A2/quicksort-par.c
@@ -167,11 +167,11 @@ int par_partition(int* Arr, int ptr_offset, int ptr_limit, int active_dimension)
 
    // select pivot - median
    {
-      int* tmp;
-      tmp = (int*)malloc(sizeof(int)*(size));
+      printf("%d: selecting local pivot\n", taskid);
+      int tmp[];
       memcpy(tmp, Arr + ptr_offset, sizeof(int)*(size));
       local_pivot = randomized_select(tmp, 0, size - 1, size / 2);
-      free(tmp);
+      printf("%d: local pivot is %d\n", taskid, local_pivot);
    }
 
    // take average of pivots
